c4b1600c1b35b74d0460812c718968e9e4d010f6
virath.liyanage@gmail.com <virath.liyanage@gmail.com@b61e6f3f-8efc-d4ce-ac76-55cc3e6106d2>
2010-12-26 15:09:22 +0000

    Port has been changed

--- branches/Notification_Test_Module/Notification_Test_Module/Default.aspx.cs
+++ branches/Notification_Test_Module/Notification_Test_Module/Default.aspx.cs
@@ -9,20 +9,21 @@ namespace Notification_Test_Module
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 25);
-
-            NetworkCredential info = new NetworkCredential("virath.liyanage@gmail.com", "ThePassword");// Type your Email and the password
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 465);//209.85.227.109
 
             smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-
-            smtpClient.Credentials = info;
+            smtpClient.UseDefaultCredentials = false;
             smtpClient.EnableSsl = true;
+
+            NetworkCredential info = new NetworkCredential("virath.liyanage@gmail.com", "Password");// Type your Email and the password
+            smtpClient.Credentials = info;
 
             MailMessage mail = new MailMessage();
 
-            mail.From = new MailAddress("virath.liyanage@gmail.come", "Resource Management System");
+            mail.From = new MailAddress("virath.liyanage@gmail.com", "Resource Management System");
             mail.To.Add(new MailAddress("jeevantha@gmail.com"));
-            mail.CC.Add(new MailAddress("virath.liyanage@gmail.come"));
+            mail.To.Add(new MailAddress("gayanpw@gmail.com"));
+            mail.CC.Add(new MailAddress("virath.liyanage@gmail.com"));
 
             StringBuilder mailBody = new StringBuilder();
 
