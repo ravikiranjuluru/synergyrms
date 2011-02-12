a5c0c394b6fc9fa6a3470aec3c483ad5837f2a2f
srimalit <srimalit@gmail.com>
2011-02-12 17:15:23 +0000

    Added comments

--- branches/SourceCode/ServiceLayer/ServiceResources.cs
+++ branches/SourceCode/ServiceLayer/ServiceResources.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+// Exposing service leyer for extended functionalities
+
 namespace ServiceLayer
 {
     class ServiceResources
